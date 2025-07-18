81fb4b90d05f1d921fc691b3e6a5ae246a6a3a80
Prakash Chandra sahoo <110497983+prakasham-om@users.noreply.github.com>
2025-07-18 13:46:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: 'https://gravity-six-theta.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   },
 });
